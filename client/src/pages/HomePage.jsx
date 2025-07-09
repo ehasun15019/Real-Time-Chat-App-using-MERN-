a843ad8af7bef5dd89bfcf7005abb6965b9c4c8d
Your Full Name <ehasun0909@gamil.com>
2025-07-10 02:33:16 +0600

    pages, components, hooks

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import SideBar from "../components/SideBar";
+import ChatContainer from "../components/ChatContainer";
+import RightSidebar from "../components/RightSidebar";
 
 const HomePage = () => {
+  // when user is not selected then don't show the RightSide bar this is the reason for this state
+  const [selectedUser, setSelectedUser] = useState(false);
+
   return (
-    <div>
-      <p>This is home</p>
+    <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
+      <div
+        className={`backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative 
+          ${
+            selectedUser
+              ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]"
+              : "md:grid-cols-2"
+          }`}
+      >
+        <SideBar />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
     </div>
   );
 };
