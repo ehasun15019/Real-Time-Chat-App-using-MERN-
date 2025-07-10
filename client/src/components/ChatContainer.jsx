ad0b3bb1b19f5dd43cb46c518fb0c53119d55c2f
Your Full Name <ehasun0909@gamil.com>
2025-07-10 12:48:57 +0600

    started working with ChatContainer

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import assets from "../assets/assets";
 
 const ChatContainer = () => {
   return (
     <div>
-      <h1>ChatContainer</h1>
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
+        <img
+          src={assets.profile_martin}
+          alt="profile image"
+          className="w-8 rounded-full"
+        />
+
+        <p className="flex-1 text-lg text-white flex items-center gap-2">
+          Martin Jonson{" "}
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+      </div>
     </div>
   );
 };
