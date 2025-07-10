93be0ddefdfb4147ac799acb3f6ec306a18db458
Your Full Name <ehasun0909@gamil.com>
2025-07-10 15:37:40 +0600

    selected User for  ternary with ChaContainer.jsx

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import assets from "../assets/assets";
 
-const ChatContainer = () => {
-  return (
-    <div>
+// get the props
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+  return selectedUser ? (
+    <div className="h-full overflow-scroll relative backdrop-blur-lg">
       <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-500">
         <img
           src={assets.profile_martin}
@@ -15,7 +16,25 @@ const ChatContainer = () => {
           Martin Jonson{" "}
           <span className="w-2 h-2 rounded-full bg-green-500"></span>
         </p>
+
+        <img
+          src={assets.arrow_icon}
+          alt="arrow icon"
+          className="md:hidden max-w-7"
+          onClick={() => setSelectedUser(null)}
+        />
+
+        <img
+          src={assets.help_icon}
+          alt="info icon"
+          className="max-md:hidden max-w-5"
+        />
       </div>
+    </div>
+  ) : (
+    <div className="flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden">
+      <img src={assets.logo_icon} className="max-w-16" alt="logo icon" />
+      <p className="text-lg font-medium text-white">Chat anytime, anywhere</p>
     </div>
   );
 };
